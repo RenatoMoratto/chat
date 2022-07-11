4774e145372fa870508a5c242980d3595ae3695f
Renato Willyan Moratto Filho <renatowmoratto@gmail.com>
2022-07-11 16:30:46 -0300

    feat: auth page interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:chat/pages/loading_page.dart';
 import 'package:flutter/material.dart';
+
+import 'pages/auth_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoadingPage(),
+      home: const AuthPage(),
       debugShowCheckedModeBanner: false,
     );
   }
