d85cd21e5f8c5b48bf4df3caaa7683a40bb7f40d
Renato Willyan Moratto Filho <renatowmoratto@gmail.com>
2022-07-13 12:02:06 -0300

    feat: initialize firebase app

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import '../core/services/auth/auth_service.dart';
@@ -9,21 +10,31 @@ import 'loading_page.dart';
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({Key? key}) : super(key: key);
 
+  Future<void> init(BuildContext context) async {
+    await Firebase.initializeApp();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<ChatUser?>(
-        stream: AuthService().userChanges,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const LoadingPage();
-          }
-          if (snapshot.hasData) {
-            return const ChatPage();
-          }
-          return const AuthPage();
-        },
-      ),
+    return FutureBuilder(
+      future: init(context),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const LoadingPage();
+        }
+        return StreamBuilder<ChatUser?>(
+          stream: AuthService().userChanges,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const LoadingPage();
+            }
+            if (snapshot.hasData) {
+              return const ChatPage();
+            }
+            return const AuthPage();
+          },
+        );
+      },
     );
   }
 }
